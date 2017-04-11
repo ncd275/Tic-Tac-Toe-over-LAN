ce5597a532877d48069f37132e0fdc6c2c0cc78f
Nick Dangles <ncdangl@ilstu.edu>
2017-04-10 23:17:02 -0500

    initial commit, still fixing minor details

---
+++ TTTOL/player2.py
@@ -0,0 +1,33 @@
+#Tic-Tac-Toe over LAN
+#player2.py acts as the client
+import socket             
+
+print 'Welcome to Tic-Tac-Toe over LAN\nYou are player 2 acting as the client\n\nEnter the private IP address of player 1: '
+host=raw_input()
+
+s = socket.socket() 
+port = 12345               
+
+s.connect((host, port))
+
+
+
+print s.recv(4096)
+playername=raw_input()
+s.send(playername)
+print '\nWaiting for player 1...'
+print s.recv(4096)
+while True:
+	data=s.recv(4096)
+	if 'piece' in data:
+		print data
+		position=raw_input()
+		s.send(position)
+	else:
+		print data
+	if data=='Finished':
+		break
+
+s.close()
+
+              
